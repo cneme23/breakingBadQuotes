885bbb1ee3ca2bf032b4d18d569361b8a19ff05c
carlos neme <carlosneme23@gmail.com>
2021-06-01 13:47:46 -0300

    Adding changes

--- breaking-bad/src/App.js
+++ breaking-bad/src/App.js
@@ -1,9 +1,38 @@
 import logo from './logo.svg';
+import styled from "@emotion/styled";
+
+
+const Contenedor = styled.div`
+display: flex;
+align-items: center;
+padding-top: 5rem;
+flex-direction: column;
+ `;
+
+const Boton = styled.button`
+background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40% , #0f574e 100% );
+background-size: 300px;
+font-family: Arial, Helvetica, sans-serif;
+color:#fff;
+margin-top:3rem;
+padding: 1rem 3rem;
+font-size: 2rem;
+border:2 px solid black;
+
+ `;
 
 
 function App() {
+const consultarAPI = () => {
+  console.log("consultando...")
+}
+
   return (
-    <h1>Breaking Bad</h1>
+    <Contenedor>
+      <Boton onClick={consultarAPI}>
+        Obtener frase
+      </Boton>
+    </Contenedor>
   );
 }
 
