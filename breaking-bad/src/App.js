2777a4dae828cb0e0923fec36b7e04a366933915
carlos neme <carlosneme23@gmail.com>
2021-06-02 02:35:41 -0300

    adding changes

--- breaking-bad/src/App.js
+++ breaking-bad/src/App.js
@@ -8,30 +8,24 @@ const Contenedor = styled.div`
   align-items: center;
   padding-top: 5rem;
   flex-direction: column;
-`;
+` ;
 
 const Boton = styled.button`
-  background: -webkit-linear-gradient(
-    top left,
-    #007d35 0%,
-    #007d35 40%,
-    #0f574e 100%
-  );
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
   background-size: 300px;
-  font-family: Arial, Helvetica, sans-serif;
+  font-family:  Arial, Helvetica, sans-serif;
   color: #fff;
   margin-top: 3rem;
   padding: 1rem 3rem;
   font-size: 2rem;
-  border: 2 px solid black;
-  transition:background-size .8s ease;
-  
-  :hover{
+  border: 2px solid black;
+  transition: background-size .8s ease;
+  :hover {
     cursor:pointer;
-    background-size: 400;
+    background-size: 400px;
   }
-
 `;
+
 
 function App() {
   //state de frases
---
+++ breaking-bad/src/components/frase2.js
@@ -0,0 +1,68 @@
+// const ContenedorFrase = styled.div`
+//  padding:3rem;
+//  border-radius:.5rem;
+//  background-color: #fff;
+//  max-width: 800px;
+
+// @media (min-width:992px){
+//     margin-top: 10rem;
+
+// }
+
+// h1 {
+//     font-family: Arial, Helvetica, sans-serif;
+//     text-align: center;
+//     position: relative;
+//     padding-left: 4rem;
+
+//  &::before {
+//      content:open-quote;
+//      font-size: 10rem;
+//      color:black;
+//      position:absolute;
+//      left:-1rem;
+//      top:-2rem;
+
+//  }
+
+// }
+// p{
+//     font-family: Verdana, Geneva, Tahoma, sans-serif,;
+//     font-size:1.6rem;
+//     text-align:right;
+//     color:#666;
+//     margin-top:2rem;
+// }
+
+//  `;
+
+
+// const Contenedor = styled.div`
+//   display: flex;
+//   align-items: center;
+//   padding-top: 5rem;
+//   flex-direction: column;
+// `;
+
+// const Boton = styled.button`
+//   background: -webkit-linear-gradient(
+//     top left,
+//     #007d35 0%,
+//     #007d35 40%,
+//     #0f574e 100%
+//   );
+//   background-size: 300px;
+//   font-family: Arial, Helvetica, sans-serif;
+//   color: #fff;
+//   margin-top: 3rem;
+//   padding: 1rem 3rem;
+//   font-size: 2rem;
+//   border: 2 px solid black;
+//   transition:background-size .8s ease;
+  
+//   :hover{
+//     cursor:pointer;
+//     background-size: 400;
+//   }
+
+// `;
